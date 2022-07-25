f5c1f610f2be74ac65d2fcee779d8d391d6a6f72
Claudio Jolowicz <mail@claudiojolowicz.com>
2022-07-25 10:29:09 +0200

    feat: exercise 1.4

--- ch1/dup2/main.go
+++ ch1/dup2/main.go
@@ -1,40 +1,51 @@
 // Dup2 prints the count and text of lines that appear more than once
-// in the input.  It reads from stdin or from a list of named files.
+// in the input.  It reads from stdin or from a list of named args.
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "os"
+	"bufio"
+	"fmt"
+	"os"
 )
 
+type linemap = map[string]map[string]bool
+
 func main() {
-    counts := make(map[string]int)
-    files := os.Args[1:]
-    if len(files) == 0 {
-        countLines(os.Stdin, counts)
-    } else {
-        for _, arg := range files {
-            f, err := os.Open(arg)
-            if err != nil {
-                fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
-                continue
-            }
-            countLines(f, counts)
-            f.Close()
-        }
-    }
-    for line, n := range counts {
-        if n > 1 {
-            fmt.Printf("%d\t%s\n", n, line)
-        }
-    }
+	lines := make(linemap)
+	args := os.Args[1:]
+	if len(args) == 0 {
+		groupFilesByLine(os.Stdin, lines, "<stdin>")
+	} else {
+		for _, arg := range args {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			groupFilesByLine(f, lines, arg)
+			f.Close()
+		}
+	}
+	for line, files := range lines {
+		if len(files) > 1 {
+			fmt.Printf("%s\n", line)
+			for match := range files {
+				fmt.Printf("\t%s\n", match)
+			}
+		}
+	}
 }
 
-func countLines(f *os.File, counts map[string]int) {
-    input := bufio.NewScanner(f)
-    for input.Scan() {
-        counts[input.Text()]++
-    }
-    // NOTE: ignoring potential errors from input.Err()
+func groupFilesByLine(f *os.File, lines linemap, file string) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		line := input.Text()
+		files, ok := lines[line]
+		if !ok {
+			files = make(map[string]bool)
+			lines[line] = files
+		}
+		files[file] = true
+	}
+	// NOTE: ignoring potential errors from input.Err()
 }
